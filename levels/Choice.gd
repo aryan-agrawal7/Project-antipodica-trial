27f40d5dd4117a299ca2c3dccc30b279f1ef6ec5
Aryan <aryan.a@research.iiit.ac.in>
2026-02-21 22:06:24 +0530

    Trial

---
+++ levels/Choice.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+@onready var suit_buttons = $VBoxContainer/HBoxContainer2/Button.button_group
+@onready var ship_buttons = $VBoxContainer/HBoxContainer/Button.button_group
+
+func _on_start_pressed() -> void:
+	var selected_suit = suit_buttons.get_pressed_button().text
+	var selected_ship = ship_buttons.get_pressed_button().text
+	get_tree().change_scene_to_file("res://levels/level_2.tscn.tscn")
