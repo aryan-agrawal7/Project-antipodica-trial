a4ac43c77cfc34214f4173ade3562b638a1308f2
Pranav-S05 <pranav.srivastava@research.iiit.ac.in>
2026-02-21 22:07:24 +0530

    Global Variable

--- Scripts/Choice.gd
+++ Scripts/Choice.gd
@@ -6,4 +6,6 @@ extends CanvasLayer
 func _on_start_pressed() -> void:
 	var selected_suit = suit_buttons.get_pressed_button().text
 	var selected_ship = ship_buttons.get_pressed_button().text
+	GameDetails.ShipSelected = selected_ship
+	GameDetails.SuitSelected = selected_suit
 	get_tree().change_scene_to_file("res://levels/level_2.tscn.tscn")
