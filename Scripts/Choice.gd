391e8ece757d2e30e0040e55613b3686df395c68
Pranav-S05 <pranav.srivastava@research.iiit.ac.in>
2026-04-11 02:13:27 +0530

    trail 2 here

--- Scripts/Choice.gd
+++ Scripts/Choice.gd
@@ -3,10 +3,4 @@ extends CanvasLayer
 @onready var suit_buttons = $VBoxContainer/HBoxContainer2/Button.button_group
 @onready var ship_buttons = $VBoxContainer/HBoxContainer/Button.button_group
 #hahah
-
-func _on_start_pressed() -> void:
-	var selected_suit = suit_buttons.get_pressed_button().text
-	var selected_ship = ship_buttons.get_pressed_button().text
-	GameDetails.ShipSelected = selected_ship
-	GameDetails.SuitSelected = selected_suit
-	get_tree().change_scene_to_file("res://levels/level_2.tscn.tscn")
+#second commit
